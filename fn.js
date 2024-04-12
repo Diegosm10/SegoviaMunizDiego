fe86417c0d6e8e7cbc09ef6b050c070e588700d1
Diegosm10 <107966736+Diegosm10@users.noreply.github.com>
2024-04-11 21:36:56 -0300

    Add files via upload

---
+++ fn.js
@@ -0,0 +1,17 @@
+/* let es como var pero vive en una parte del codigo */
+/*let frutas = ['Pera','Manzana', 'Frutilla', 'Naranja', 'Mandarina'];*/
+/*for (i=0; i <= frutas.length; i++) {
+    console.log(frutas[i])
+}*/
+/* forEach no utiliza indice i y devuelve una funcion 
+frutas.forEach(fruta => {
+    console.log(fruta);
+});
+console.log(array[1])*/
+/*
+for (const fruta of frutas){
+    if (fruta == 'Manzana')
+    console.log(fruta);
+}*/
+/* === compara y compara el tipo de dato */
+
