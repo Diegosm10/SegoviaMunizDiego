bf9eba44d5d4da2ab7a823d556ce156137ecac0f
Diegosm10 <diego666.segovia777@gmail.com>
2024-04-13 15:16:27 -0300

    TP2 actualizado

--- TP2/json.js
+++ TP2/json.js
@@ -1,11 +1,18 @@
 function buscadorpersonajes() {
     var numero = document.getElementById("id").value;
     var numero2 = document.getElementById("id2").value;
-    fetch(`https://rickandmortyapi.com/api/character/${numero}/${numero2}`)
+    fetch(`https://rickandmortyapi.com/api/character/${numero},${numero2}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        document.getElementById("nombre").innerHTML = data.name;
-        document.getElementById("nombre2").innerHTML = data.name;
+        for (i=0; i <= data.length; i++) {
+            if (data[i].id == numero){
+                document.getElementById("nombre").innerHTML = data[i].name;
+                document.getElementById("foto1").innerHTML = `<img src="${data[i].image}" alt="foto personaje">`
+            }
+            if (data[i].id ==  numero2) {
+                document.getElementById("nombre2").innerHTML = data[i].name;
+                document.getElementById("foto2").innerHTML = `<img src="${data[i].image}" alt="foto personaje">`
+            }
+        }
     });
 }
