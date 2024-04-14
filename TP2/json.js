e0a53a2bd045f908788bc2cc9c965f9d9d440b53
Diegosm10 <diego666.segovia777@gmail.com>
2024-04-14 13:22:52 -0300

    TP2 opcion1

--- TP2/json.js
+++ TP2/json.js
@@ -8,10 +8,19 @@ function buscadorpersonajes() {
             if (data[i].id == numero){
                 document.getElementById("nombre").innerHTML = data[i].name;
                 document.getElementById("foto1").innerHTML = `<img src="${data[i].image}" alt="foto personaje">`
+                var genero1 = data[i].gender;
             }
+            else
             if (data[i].id ==  numero2) {
                 document.getElementById("nombre2").innerHTML = data[i].name;
                 document.getElementById("foto2").innerHTML = `<img src="${data[i].image}" alt="foto personaje">`
+                var genero2 = data[i].gender;
+            }
+            if (genero1 == genero2) {
+                document.getElementById("match").innerHTML = `<img src="https://imgs.search.brave.com/8cbjpdIb4z5FQfoUA1eZdNxAomFUBtkHZyhT5kllze0/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9pbWcu/ZnJlZXBpay5jb20v/cHNkLWdyYXRpcy9z/aW1ib2xvLXZlcmlm/aWNhY2lvbi1haXNs/YWRvXzIzLTIxNTA1/MDAzNTYuanBnP3Np/emU9NjI2JmV4dD1q/cGc" alt="verificacion ok" width="100" height="100">`
+            }
+            if (genero1 != genero2) {
+                document.getElementById("match").innerHTML = `<img src="https://imgs.search.brave.com/X8Ih00I_9AVUJvT_vimeYE2ZlkEibbaSXiiW8P7mTF4/rs:fit:860:0:0/g:ce/aHR0cHM6Ly9jZG4u/cGl4YWJheS5jb20v/cGhvdG8vMjAxNy8w/Mi8xMi8yMS8yOS9m/YWxzZS0yMDYxMTMx/XzY0MC5wbmc" alt="verificacion x" width="100" height="100">`
             }
         }
     });
